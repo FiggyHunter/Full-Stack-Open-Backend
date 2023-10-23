6eae6c64750570461c66cafcef5d6281b71bcef6
FiggyHunter <leonardo.roi3@gmail.com>
2023-10-23 14:13:31 +0200

    Completed Exercise 4.5*

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -10,4 +10,13 @@ const totalLikes = (blogPosts) => {
   return sum;
 };
 
-export default { dummy, totalLikes };
+const favoriteBlog = (blogPosts) => {
+  // Use reduce to find the object with the most likes, and destructure the properties of that object
+  const { author, likes, title } = blogPosts.reduce((max, obj) => {
+    return obj.likes > max.likes ? obj : max;
+  }, blogPosts[0]);
+  // Create and return an object with the author, likes, and title of the favorite blog, based on the above generated object
+  return { author, likes, title };
+};
+
+export default { dummy, totalLikes, favoriteBlog };
