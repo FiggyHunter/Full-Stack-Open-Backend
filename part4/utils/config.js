837d5de51f594c6b05cadad2419f46da5575da32
FiggyHunter <leonardo.roi3@gmail.com>
2023-10-24 15:57:11 +0200

    config now includes the test db url

--- part4/utils/config.js
+++ part4/utils/config.js
@@ -1,2 +1,10 @@
-export const DATABASE_URL = process.env.DB_TWO;
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const DATABASE_URL =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_MONGODB_URI
+    : process.env.DB_TWO;
+
 export const PORT = 3777;
